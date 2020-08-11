6aa80ca7ab105e77822425b2e095c39268579e44
Johar-tumen <58850287+Johar-tumen@users.noreply.github.com>
2020-08-11 17:09:00 +0500

    start

--- src/js/blocks/blocks.js
+++ src/js/blocks/blocks.js
@@ -1,9 +1,6 @@
-
 import $ from "jquery";
 
 export const headerBlock = $("#header");
-export const aboutBlock = $("#about");
-export const productsBlock = $("#products");
-export const membersBlock = $("#members");
+export const galleryBlock = $("#gallery");
 export const footerBlock = $("#footer");
 
